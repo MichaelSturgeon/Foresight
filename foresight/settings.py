b24f1ac2e6f5b2cd6fd56608c9447006690b61e4
Michael Sturgeon <1993msturgeon@gmail.com>
2024-10-03 21:22:08 +0000

    Create 'Post' model

--- foresight/settings.py
+++ foresight/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-michaelstur-pp4foresigh-i592upc8362.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'foresight.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
--- social/models.py
+++ social/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name="posts", on_delete=models.DO_NOTHING)
+    body = models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.user} | {self.created_on:%Y-%m-%d %H:%M} | {self.body}"
+         
