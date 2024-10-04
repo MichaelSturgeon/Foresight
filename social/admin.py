add8427c9924a89c57f8e7caf73c4ea8e6541611
Michael Sturgeon <1993msturgeon@gmail.com>
2024-10-04 15:34:55 +0000

    Merge 'Profile' model & 'User' model in admin panel, and create user profile upon sign-up

--- social/admin.py
+++ social/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .models import Post, Profile
+from django.contrib.auth.models import User
+from .models import Profile, Post
 
-# Register your models here.
+# Conbine Profile Model & User Model
+class ProfileInline(admin.StackedInline):
+	model = Profile
+
+# Modify User Model
+class UserAdmin(admin.ModelAdmin):
+	model = User
+	inlines = [ProfileInline]
+
+# Unregister User Model
+admin.site.unregister(User)
+
+# Register User Model
+admin.site.register(User, UserAdmin)
+
+# Register Models
 admin.site.register(Post)
-admin.site.register(Profile)
--- social/models.py
+++ social/models.py
@@ -1,9 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
 from django.contrib.auth.models import User
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Create your models here.
+# Create post
 class Post(models.Model):
     user = models.ForeignKey(User, related_name="posts", on_delete=models.DO_NOTHING)
     body = models.CharField(max_length=200)
@@ -14,9 +17,18 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.user} | {self.created_on:%Y-%m-%d %H:%M} | {self.body}"
 
-
+# Create A user profile
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
     def __str__(self):
         return f"{self.user.username}"
+
+# Create A profile when a user signs-up & automatically follows own profile
+@receiver(post_save, sender=User)
+def create_user_profile(created, instance, sender, **kwargs):
+    if created:
+        created_profile = Profile(user=instance)
+        created_profile.save()
+        created_profile.follows.set([instance.profile.id])
+        created_profile.save()
