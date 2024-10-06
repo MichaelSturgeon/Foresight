ff9cb2107f27e291db9d3106b5b9b0ee64027384
Michael Sturgeon <1993msturgeon@gmail.com>
2024-10-06 17:47:39 +0000

    Add page pagination to 'index.html'

--- social/views.py
+++ social/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Post, Profile
 
 # Create your views here.
 def home(request):
-    posts = Post.objects.all() 
-    return render(request, 'social/index.html', {"posts":posts})
+    # Paginate post list
+    paginator = Paginator(Post.objects.all(), 10)
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+
+
+    return render(request, 'social/index.html', {
+        "posts":posts,
+    })
 
 
 def profile_list(request):
