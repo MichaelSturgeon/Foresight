177c6f121197958b082bc12e1ab5d5506a1a7a5b
Michael Sturgeon <1993msturgeon@gmail.com>
2024-10-06 12:29:50 +0000

    Display list of user profiles on 'profile_list.html' & add basic bootstrap styling with a 'static' placeholder image

--- social/views.py
+++ social/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Profile
 
 # Create your views here.
 def home(request):
     posts = Post.objects.all() 
-    return render(request, 'social/home.html', {"posts":posts})
+    return render(request, 'social/index.html', {"posts":posts})
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, 'social/profile_list.html', {"profiles":profiles})
