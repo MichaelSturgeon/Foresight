54ec5f0b49e8213df925c0c35b973aa75a10971a
Michael Sturgeon <1993msturgeon@gmail.com>
2024-10-09 12:12:00 +0000

    Create follow/ unfollow button in 'my_profile.html' file and wire up to database

--- social/views.py
+++ social/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from .models import Post, Profile
 
-# Create your views here.
+# Index View
 def home(request):
     # Paginate post list
     paginator = Paginator(Post.objects.all(), 10)
@@ -15,6 +15,7 @@ def home(request):
     })
 
 
+# Profile List View
 def profile_list(request):
     if request.user.is_authenticated:
 
@@ -26,12 +27,23 @@ def profile_list(request):
         return redirect('home')
 
 
+# My Profile View
 def my_profile(request, pk):
     if request.user.is_authenticated:
 
         my_posts = Post.objects.filter(user_id=pk).order_by("-created_on")
-
         my_profile = Profile.objects.get(user_id=pk)
+
+        # Follow/ Unfollow form request
+        if request.method == "POST":
+            user_profile = request.user.profile
+            action = request.POST["follow"]
+            if action == "unfollow":
+                user_profile.follows.remove(my_profile)
+            else:
+                user_profile.follows.add(my_profile)
+            user_profile.save()
+
 
         return render(request, 'social/my_profile.html', {
             "my_profile":my_profile,
